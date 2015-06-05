beb8cd23f1a9ce521c53ac47c1df06442bdbd587
Janario Oliveira <janario.oliveira@tecsinapse.com.br>
2015-06-05 00:02:51 -0300

    Change logback-default.xml relative path

--- src/main/java/me/janario/logback/LogbackLoggerManager.java
+++ src/main/java/me/janario/logback/LogbackLoggerManager.java
@@ -1,9 +1,8 @@
 package me.janario.logback;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.logging.LogManager;
 
 import me.janario.logback.deployment.LogbackContextSelector;
@@ -11,12 +10,11 @@ import me.janario.logback.deployment.LogbackContextSelector;
 /**
  * Just to override org.jboss.logmanager.LogManager so LogManager won't be initialized
  * Once it is the first class loaded sets some properties
- *
+ * <p>
  * see /META-INF/services/java.util.logging.LogManager
- * @see org.jboss.as.server.Main
- *
  *
  * @author Janario Oliveira
+ * @see org.jboss.as.server.Main
  */
 public class LogbackLoggerManager extends LogManager {
     public LogbackLoggerManager() {
@@ -26,16 +24,15 @@ public class LogbackLoggerManager extends LogManager {
     private void configure() {
         String logging = System.getProperty("logging.configuration");
         try {
-            Path loggingProperties = Paths.get(new URI(logging));
-            Path logbackDefault = loggingProperties.getParent().resolve("logback-default.xml");
+            final String path = new URI(logging).getPath();
+            File logbackDefault = new File(new File(path).getParent(), "logback-default.xml");
 
             //force to avoid custom modules to change auto detection
             System.setProperty("org.jboss.logging.provider", "slf4j");
             System.setProperty("logback.ContextSelector", LogbackContextSelector.class.getName());
-            System.setProperty("logback.configurationFile", logbackDefault.toFile().getPath());
+            System.setProperty("logback.configurationFile", logbackDefault.getPath());
         } catch (URISyntaxException e) {
             throw new RuntimeException("Could not find file " + logging);
         }
-
     }
 }
